8864e31b64420446d1475fca0d95c50feaeb4345
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-08-23 01:40:11 +0600

    finished

---
+++ test.js
@@ -0,0 +1,99 @@
+let player_1 = document.getElementById('playerName-1'); // name of each player fetch
+let player_2 = document.getElementById('playerName-2');
+let player_3 = document.getElementById('playerName-3');
+let player_4 = document.getElementById('playerName-4');
+let player_5 = document.getElementById('playerName-5');
+let player_6 = document.getElementById('playerName-6');
+
+let playerBtn_1 = document.getElementById('playerBtn-1'); // button of each player fetch
+let playerBtn_2 = document.getElementById('playerBtn-2');
+let playerBtn_3 = document.getElementById('playerBtn-3');
+let playerBtn_4 = document.getElementById('playerBtn-4');
+let playerBtn_5 = document.getElementById('playerBtn-5');
+let playerBtn_6 = document.getElementById('playerBtn-6');
+
+let perPlayerField = document.getElementById('perPlayerField'); //input fields
+let managerField = document.getElementById('managerField');
+let coachField = document.getElementById('coachField');
+
+let playerCost = document.getElementById('playerCost'); // cost fields
+let totalCost = document.getElementById('totalCost');
+let playerCostBtn = document.getElementById('playerCostBtn');
+let totalCostBtn = document.getElementById('totalCostBtn');
+
+let playerList = document.getElementById('playerList'); // Ol list
+
+function enterAChild(element, textVariable, whereToShow){   // entering element to list
+  let node = document.createElement(element);
+  let textNode = document.createTextNode(textVariable.innerText);
+  node.appendChild(textNode);
+  document.getElementById(whereToShow).appendChild(node);
+}
+
+function isMore5times(count){   // function to check 5 times
+  if(count >= 6){
+    alert("You have selected five player");
+    e.stopPropagation();
+  }
+}
+
+let counter = 0;
+
+// event lisner for player starts
+
+playerBtn_1.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_1, 'playerList');
+  playerBtn_1.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_1.disabled = true;
+})
+playerBtn_2.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_2, 'playerList');
+  playerBtn_2.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_2.disabled = true;
+})
+playerBtn_3.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_3, 'playerList');
+  playerBtn_3.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_3.disabled = true;
+})
+playerBtn_4.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_4, 'playerList');
+  playerBtn_4.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_4.disabled = true;
+})
+playerBtn_5.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_5, 'playerList');
+  playerBtn_5.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_5.disabled = true;
+})
+playerBtn_6.addEventListener('click', function(){
+  counter += 1;
+  isMore5times(counter);
+  enterAChild('li', player_6, 'playerList');
+  playerBtn_6.className = 'bg-blue-500 text-white font-bold py-2 px-4 opacity-50 cursor-not-allowed rounded-lg px-5 py-2.5 text-center mr-2 mb-2';
+  playerBtn_6.disabled = true;
+})
+
+// event lisner for player end
+
+// calculation start
+playerCostBtn.addEventListener('click', function(){
+  playerCost.innerText = perPlayerField.value * 5;
+})
+totalCostBtn.addEventListener('click', function(){
+  let manager = parseInt(managerField.value);
+  let coach = parseInt(coachField.value);
+  let perPlayer = parseInt(playerCost.innerText);
+  totalCost.innerText = manager + coach + perPlayer;
+})
+// calculation end
